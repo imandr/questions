e82073038f856221296b813adb69526770b6152e
Igor Mandrichenko <igorvm@Igors-MacBook-Pro.local>
2017-04-18 09:13:18 -0500

    added extra dense layer

--- model/model.py
+++ model/model.py
@@ -17,28 +17,30 @@ def last(x):
 
 def createModel(row_size, nout):
     
+    inner = int(row_size * 1.5)
+    
     in1 = Input(shape=(None, row_size))
     in2 = Input(shape=(None, row_size))
     
-    lstm_a = LSTM(row_size, return_sequences=True, implementation=1,  name="lstm_a")
+    lstm_a = LSTM(inner, return_sequences=True, implementation=1,  name="lstm_a")
     lstm_a_1 = lstm_a(in1)
     lstm_a_2 = lstm_a(in2)
 
     last_a_1 = Lambda(last, name="last_a_1")(lstm_a_1)
     last_a_2 = Lambda(last, name="last_a_2")(lstm_a_2)
     
-    lstm_b = LSTM(row_size, return_sequences=False, implementation=1,  name="lstm_b")
+    lstm_b = LSTM(inner, return_sequences=False, implementation=1,  name="lstm_b")
     lstm_b_1 = lstm_b(lstm_a_1)
     lstm_b_2 = lstm_b(lstm_a_2)
     
-    merge = Dense(row_size, name="merge_dense", activation="tanh")
+    merge = Dense(inner, name="merge_dense", activation="tanh")
     
     merge_1 = merge(concatenate([last_a_1, lstm_b_1], name="merge_1"))
     merge_2 = merge(concatenate([last_a_2, lstm_b_2], name="merge_2"))
     
     merged = concatenate([merge_1, merge_2], name="merge_12")
-    #dense = Dense(row_size)(merged)
-    out = Dense(nout, activation=Activation("softmax"), name="output")(merged)
+    dense = Dense(nout*10, activation="tanh")(merged)
+    out = Dense(nout, activation=Activation("softmax"), name="output")(dense)
     model = Model(inputs=[in1, in2], outputs=out)
     model.compile(loss='categorical_crossentropy', optimizer=Adadelta(), metrics=["accuracy"])
     return model
